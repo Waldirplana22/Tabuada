d6f5306963b71cc8b0198655434b339920a00170
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-06-19 22:00:05 -0300

    Tabuada

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program {
+  public static void Main (string[] args) {
+  
+            
+            Console.WriteLine("---- Multiplicando a Tabuada ----");
+            PularLinha();
+            
+
+            int tabuada, contador = 0;
+
+            Console.Write("Qual número deseja para a tabuada..: ");
+            tabuada = Convert.ToInt32(Console.ReadLine());
+            PularLinha();
+
+            Console.WriteLine($"A tabuada que escolheu é do {tabuada}");
+
+            do
+            {
+                Console.Write($" {tabuada} x {contador} = ");
+                Console.WriteLine(tabuada * contador);
+                contador = contador + 1;
+                
+            }
+            while(contador <=10);          
+        }
+
+        static void ResetarCor()
+        {
+            Console.ResetColor();
+        }
+
+        static void LimparTela()
+        {
+            Console.Clear();
+        }
+
+        static void PularLinha()
+        {
+            Console.WriteLine();
+        }
+    }
